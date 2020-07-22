a147d545794ccdcb6a3d946ee4898b00c37fd84d
jamesc2000 <cruz.james99@gmail.com>
2020-07-22 13:39:22 +0800

    + Timer added + Controls added

--- js/renderer.js
+++ js/renderer.js
@@ -1,4 +1,15 @@
-// Put all things related to rendering 
+// Put all things related to rendering
 // things in the browser here
 
 // Timer
+let timer = document.querySelector("#timer");
+let timeLeft = 3;
+var time = setInterval(function() {
+    console.log("as");
+    timer.innerHTML = timeLeft;
+    timeLeft--;
+
+    if (timeLeft < 0) {
+        clearInterval(time);
+    }
+}, 1000);
