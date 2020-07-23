2a98bd278f96c24a43566f9e6385751efa4c4254
jamesc2000 <cruz.james99@gmail.com>
2020-07-23 15:22:03 +0800

    + Added input field for name TODO: POST the name inputted in the field
    + Which player is which logic
    
    + REST api functionality added on game.js

--- js/game.js
+++ js/game.js
@@ -1,6 +1,34 @@
-// This file is for game logic, for rendering to the browser
-// go to renderer.js. Functions and variables from renderer.js
-// cannot be used here
+// This file is ran by the server, this opens up endpoints
+// for the rest api and also handles game state and logic
 
-var playerHealth = 20
+console.log("Server running");
 
+let express = require("express");
+let cors = require("cors");
+let bodyParser = require("body-parser");
+let server = express();
+
+let players = [];
+
+server.use(cors());
+server.use(bodyParser.json({ extended: true }));
+
+server.post("/join", (req, res) => {
+    console.log(`Player ${req.body.name} joined`);
+    players.push(req.body);
+    res.send({ pos: players.length }); // TODO: This should be a json response using res.json()
+    if (players.length == 2) {
+        console.log("Starting game");
+        let match = {
+            timestamp: Date.now(),
+            playerData: {
+                p1: players[0],
+                p2: players[1]
+            }
+        };
+        players.length = 0;
+        console.log(JSON.stringify(match));
+    }
+});
+
+server.listen(80, () => console.log("Server listening on port 80"));
--- js/renderer.js
+++ js/renderer.js
@@ -1,15 +1,73 @@
 // Put all things related to rendering
 // things in the browser here
 
+// When joining game
+function join() {
+    let playerData = {
+        name: "James"
+    };
+    let data = {
+        method: "POST",
+        mode: "cors",
+        credentials: "same-origin",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(playerData)
+    };
+    fetch("http://localhost/join", data)
+        .then(res => {
+            res.json().then(data => {
+                console.log(data.pos);
+            });
+        })
+        .catch(error => console.error("Error: ", error));
+}
+join();
+
+// When quitting game
+
 // Timer
 let timer = document.querySelector("#timer");
 let timeLeft = 3;
 var time = setInterval(function() {
-    console.log("as");
+    // console.log("as");
     timer.innerHTML = timeLeft;
     timeLeft--;
 
     if (timeLeft < 0) {
         clearInterval(time);
+        // fetch("http://localhost/", postMe)
+        //     .then(res => console.log(res))
+        //     .catch(() => console.log("Error"));
     }
 }, 1000);
+
+// Controls
+let me = document.querySelector("#me");
+let controls = document.querySelectorAll(".controls");
+controls.forEach(element => {
+    element.addEventListener("click", function() {
+        // console.log("A control was clicked! " + this.innerHTML);
+        me.innerHTML = this.innerHTML;
+    });
+});
+
+// Randomize enemy display
+let enemy = document.querySelector("#enemy");
+let curr = 0;
+var randomize = setInterval(function() {
+    // console.log("heyo " + curr);
+    enemy.innerHTML = controls[curr].innerHTML;
+    curr++;
+
+    if (curr == 3) {
+        curr = 0;
+    }
+
+    // Remove this when we have a working backend
+    // for testing purposes only
+    if (timeLeft < 0) {
+        clearInterval(randomize);
+    }
+}, 100);
