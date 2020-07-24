8bfee68a6cac46a742c1240f3497bf8924acabe5
jamesc2000 <cruz.james99@gmail.com>
2020-07-24 15:48:12 +0800

    + Match confirmation logic done

--- js/game.js
+++ js/game.js
@@ -1,6 +1,17 @@
 // This file is ran by the server, this opens up endpoints
 // for the rest api and also handles game state and logic
 
+// Game lifecycle should be
+// 1. Wait for 2 players to join
+// 2. Start game
+// 3. Evaluate win conditions
+// 4. Check if a player left the game
+
+// Game outcomes
+// 1. Player 1 win / other player left midgame
+// 2. Player 2 win / other player left midgame
+// 3. Draw/No Win
+
 console.log("Server running");
 
 let express = require("express");
@@ -16,18 +27,69 @@ server.use(bodyParser.json({ extended: true }));
 server.post("/join", (req, res) => {
     console.log(`Player ${req.body.name} joined`);
     players.push(req.body);
-    res.send({ pos: players.length }); // TODO: This should be a json response using res.json()
+
+    // function createMatch() {
+    //     return new Promise((resolve, reject) => {
+    //         let match = {
+    //             timestamp: Date.now(),
+    //             playerData: {
+    //                 p1: await players[0],
+    //                 p2: await players[1]
+    //             }
+    //         };
+    //         resolve(match)
+    //     })
+    // }
+
+    // async function createMatch() {
+    //     let match = {
+    //         timestamp: Date.now(),
+    //         playerData: {
+    //             p1: await players[0],
+    //             p2: await players[1]
+    //         }
+    //     };
+    //     return match;
+    // }
+
+    // createMatch().then(match => {
+    //     res.json(match);
+    // });
+
     if (players.length == 2) {
         console.log("Starting game");
         let match = {
-            timestamp: Date.now(),
             playerData: {
                 p1: players[0],
                 p2: players[1]
-            }
+            },
+            playerNumber: 2
         };
-        players.length = 0;
+        res.json(match);
         console.log(JSON.stringify(match));
+    } else {
+        let match = {
+            playerData: {
+                p1: players[0]
+            },
+            playerNumber: 1
+        };
+        res.json(match);
+        console.log(JSON.stringify(match));
+    }
+});
+
+server.get("/confirm", (req, res) => {
+    console.log("Get request received");
+    if (players.length == 2) {
+        let match = {
+            playerData: {
+                p1: players[0],
+                p2: players[1]
+            },
+            playerNumber: 1
+        };
+        res.json(match);
     }
 });
 
--- js/renderer.js
+++ js/renderer.js
@@ -1,10 +1,26 @@
 // Put all things related to rendering
 // things in the browser here
 
+// Local browser lifecycle
+// 1. Join game
+//    a. POST ${name}
+//    b. Wait for game confirmation (GET request server periodically, then
+//       wait for response: {player1Name, player2Name, playerNumber})
+//       * player1Name, player2Name is needed for rendering name of other player on UI
+// 2. Start timer on game start (on fulfillment of GET responses)
+// 3. Player decides on rock, paper, or scissors
+// 4. Send decision to server to be evaluated for win conditions
+//    a. POST ${decision}
+//    b. Wait for response on winner
+//    c. Display response on UI
+// 5a. Repeat
+// 5b. onBeforeUnload detects when a player leaves the game, POST this to server to cancel match
+//       * https://stackoverflow.com/questions/6895564/difference-between-onbeforeunload-and-onunload
+
 // When joining game
 function join() {
     let playerData = {
-        name: "James"
+        name: document.querySelector("#playerName").value
     };
     let data = {
         method: "POST",
@@ -18,30 +34,86 @@ function join() {
     fetch("http://localhost/join", data)
         .then(res => {
             res.json().then(data => {
-                console.log(data.pos);
+                console.log(data);
+                if (data.playerNumber == 2) {
+                    startTimer();
+                    console.log("Match confirmed");
+                } else {
+                    wait();
+                    console.log("Waiting for other player");
+                }
             });
         })
         .catch(error => console.error("Error: ", error));
 }
-join();
+let joinBtn = document.querySelector("#nameButton");
+joinBtn.addEventListener("click", function() {
+    join();
+});
+document.addEventListener("keydown", function(event) {
+    if (event.keyCode == 13) {
+        join();
+    }
+});
 
-// When quitting game
+// Wait for other player
+function wait() {
+    let data = {
+        method: "GET",
+        mode: "cors",
+        credentials: "same-origin"
+    };
+    var waitLoop = setInterval(function() {
+        console.log("Get request sent");
+        fetch("http://localhost/confirm", data)
+            .then(res => {
+                console.log("Match confirmed");
+                startTimer();
+                clearInterval(waitLoop);
+            })
+            .catch(() => {
+                console.log("Waiting for other player");
+                wait();
+            });
+    }, 7 * 1000);
+}
 
 // Timer
+let enemy = document.querySelector("#enemy");
 let timer = document.querySelector("#timer");
-let timeLeft = 3;
-var time = setInterval(function() {
-    // console.log("as");
-    timer.innerHTML = timeLeft;
-    timeLeft--;
+function startTimer() {
+    let timeLeft = 3;
+    var time = setInterval(function() {
+        // console.log("as");
+        timer.innerHTML = timeLeft;
+        timeLeft--;
 
-    if (timeLeft < 0) {
-        clearInterval(time);
-        // fetch("http://localhost/", postMe)
-        //     .then(res => console.log(res))
-        //     .catch(() => console.log("Error"));
-    }
-}, 1000);
+        if (timeLeft < 0) {
+            clearInterval(time);
+            // fetch("http://localhost/", postMe)
+            //     .then(res => console.log(res))
+            //     .catch(() => console.log("Error"));
+        }
+    }, 1000);
+
+    // Randomize enemy display
+    let curr = 0;
+    var randomize = setInterval(function() {
+        // console.log("heyo " + curr);
+        enemy.innerHTML = controls[curr].innerHTML;
+        curr++;
+
+        if (curr == 3) {
+            curr = 0;
+        }
+
+        // Remove this when we have a working backend
+        // for testing purposes only
+        if (timeLeft < 0) {
+            clearInterval(randomize);
+        }
+    }, 100);
+}
 
 // Controls
 let me = document.querySelector("#me");
@@ -52,22 +124,3 @@ controls.forEach(element => {
         me.innerHTML = this.innerHTML;
     });
 });
-
-// Randomize enemy display
-let enemy = document.querySelector("#enemy");
-let curr = 0;
-var randomize = setInterval(function() {
-    // console.log("heyo " + curr);
-    enemy.innerHTML = controls[curr].innerHTML;
-    curr++;
-
-    if (curr == 3) {
-        curr = 0;
-    }
-
-    // Remove this when we have a working backend
-    // for testing purposes only
-    if (timeLeft < 0) {
-        clearInterval(randomize);
-    }
-}, 100);
